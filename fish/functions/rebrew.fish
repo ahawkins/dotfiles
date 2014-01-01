a548d0a60433f9a34989df9f0ffeb1e38d8432b4
ahawkins <adam@hawkins.io>
2014-01-01 22:09:03 +0100

    rebrew and brewup

--- fish/functions/rebrew.fish
+++ fish/functions/rebrew.fish
@@ -2,7 +2,9 @@ function brewup
   brew update
   set -lx outdated_brews (brew outdated)
   if count $outdated_brews
-    brew update $outdated_brews
+    brew unlink $outdated_brews
+    brew uninstall $outdated_brews
+    brew install $outdated_brews
   else
     echo "Everything up to date"
   end
