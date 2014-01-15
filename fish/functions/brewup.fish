c940d97846fd0293a5f90d65b18e345023b5ac21
ahawkins <me@broadcastingadam.com>
2014-01-15 23:41:56 +0100

    Cleanup after upgrades

--- fish/functions/brewup.fish
+++ fish/functions/brewup.fish
@@ -3,6 +3,7 @@ function brewup
   set -lx outdated_brews (brew outdated)
   if count $outdated_brews
     brew upgrade $outdated_brews
+    brew cleanup
   else
     echo "Everything up to date"
   end
