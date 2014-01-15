81bcb3d54d652d0b92abd84dbc55f700adacf55e
ahawkins <adam@hawkins.io>
2014-01-15 23:28:13 +0100

    Use upgrade instead of update

--- fish/functions/brewup.fish
+++ fish/functions/brewup.fish
@@ -2,7 +2,7 @@ function brewup
   brew update
   set -lx outdated_brews (brew outdated)
   if count $outdated_brews
-    brew update $outdated_brews
+    brew upgrade $outdated_brews
   else
     echo "Everything up to date"
   end
