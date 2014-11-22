e1315a19a34268d88218c5901459dcfeda23ae9f
ahawkins <adam@hawkins.io>
2014-11-22 13:03:39 +0100

    Move per computer setup into ~/.computer

--- fish/config.fish
+++ fish/config.fish
@@ -2,26 +2,11 @@
 # if this computer hasn't been prepared before
 set -q fish_setup_done; or fish_setup
 
-# Local Bin
-set PATH "$HOME/bin" $PATH
-
-# Homebrew
-set PATH /usr/local/bin $PATH
-set PATH /usr/local/sbin $PATH
-
-# Rbenv
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-. (rbenv init -|psub)
-
-# Node stuff
-# set NODE_PATH /usr/local/lib/node_modules
-# set PATH /usr/local/share/npm/bin $PATH
-
-set AWS_CREDENTIAL_FILE "$HOME/.aws_credentials"
-
 # Allow each machine to specify unique configuration
 # information
 if test -f ~/.computer
   . ~/.computer
 end
+
+# Local Bin
+set PATH "$HOME/bin" $PATH
