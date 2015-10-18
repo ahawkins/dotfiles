a78c3519a005094dcad741296f7177fc13d560cf
Adam Hawkins <adam@hawkins.io>
2015-10-18 14:40:36 +0530

    Move commit things to gitconfig

--- fish/functions/fish_user_abbreviations.fish
+++ fish/functions/fish_user_abbreviations.fish
@@ -1,16 +1,4 @@
 function fish_user_abbreviations
 	set -U fish_user_abbreviations \
-			'g=git' \
-			'gcbf=gcfb' \
-			'gh=git head' \
-			'gl=git l' \
-			'gll=git ll' \
-			'gc=git commit -v' \
-			'gcm=git commit -m' \
-			'gf=git fetch' \
-			'gfa=git fetch --all' \
-			'gm=git merge' \
-			'gr=git rebase' \
-			'gcp=git cherry-pick' \
-			'grpo=git remote prune origin'
+			'g=git'
 end
