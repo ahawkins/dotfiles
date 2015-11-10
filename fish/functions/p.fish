53190f0ae9c81cf56b443573e68a309b65042e5a
Adam Hawkins <adam@hawkins.io>
2015-11-10 11:36:48 +0530

    Make p resilient to bad project JSON

--- fish/functions/p.fish
+++ fish/functions/p.fish
@@ -19,10 +19,27 @@ function p --description "Switch between projects"
 		switch $arg
 			case 'edit'
 				eval $EDITOR $config_file
+			case 'list'
+				jq -r 'keys | .[]' $config_file
+
+				return 1
 			case '*'
 				# NOTE: The things are evaled to expand ~ or $HOME in project paths
 				set -l project_root (eval echo (jq -er ".$arg.root" $config_file))
 				set -l tmux_name (jq -er ".$arg.session" $config_file)
+
+				# NOTE: jq returns the "null" string for missing keys. It also
+				# exits non-zero, but fish does not have an equivalent to set -e
+				# so command failures most be handled in this way.
+				if test "null" = "$project_root" ;or test "null" = "$tmux_name"
+					echo "$arg project not configured correctly" >&2
+					return 1
+				end
+
+				if not test -d "$project_root"
+					echo "$project_root is not a directory" >&2
+					return 1
+				end
 
 				tmux start-server ^&1 > /dev/null
 
