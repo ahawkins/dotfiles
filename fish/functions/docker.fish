559960e1a2286a81b1b608baaa321a37300d7198
Adam Hawkins <hi@ahawkins.me>
2016-03-25 00:32:44 +0530

    Rework docker wrapper

--- fish/functions/docker.fish
+++ fish/functions/docker.fish
@@ -1,23 +1,17 @@
 function docker
-	switch $argv[1]
-		case wipe
-			command docker stop (docker ps -a -q)
-			command docker rm -v (docker ps -a -q)
-		case reload
-			if command docker-machine active > /dev/null ^&1
-				command docker-machine restart (docker-machine active)
-				command docker-machine env (docker-machine active) | source
-			elsif test -n $DEFAULT_DOCKER_MACHINE
-				command docker-machine start $DEFAULT_DOCKER_MACHINE
-				command docker-machine env $DEFAULT_DOCKER_MACHINE | source
+	switch "$argv[1]"
+		case reboot
+			if test -n $ACTIVE_DOCKER_MACHINE
+				command docker-machine start $ACTIVE_DOCKER_MACHINE
+				command docker-machine env $ACTIVE_DOCKER_MACHINE | source
 			else
-				echo "No active docker-machine!" ^&1
+				echo "No ACTIVE_DOCKER_MACHINE" ^&1
 				return 1
 			end
 		case use
-			if test -n $argv[2]
-				set -e DEFAULT_DOCKER_MACHINE
-				set -xU DEFAULT_DOCKER_MACHINE $argv[2]
+			if set -q argv[2]
+				set -e ACTIVE_DOCKER_MACHINE
+				set -xU ACTIVE_DOCKER_MACHINE $argv[2]
 
 				command docker-machine start $argv[2]
 				command docker-machine env $argv[2] | source
@@ -26,10 +20,23 @@ function docker
 				return 1
 			end
 		case clean
-			command docker images --no-trunc | grep none | awk '{print $3}' | xargs docker rmi
-		case nuke
-			docker clean
-			docker wipe
+			command docker stop (docker ps -aq)
+			command docker rm -v (docker ps -aq)
+			command docker images --no-trunc | grep -F '<none>' | awk '{print $3}' | xargs -L1 docker rmi -f
+		case shutdown
+			if test -n $ACTIVE_DOCKER_MACHINE
+				docker-machine stop $ACTIVE_DOCKER_MACHINE
+			else
+				echo "No ACTIVE_DOCKER_MACHINE" ^&1
+				return 1
+			end
+		case ip
+			if test -n $ACTIVE_DOCKER_MACHINE
+				docker-machine ip $ACTIVE_DOCKER_MACHINE
+			else
+				echo "No ACTIVE_DOCKER_MACHINE" ^&1
+				return 1
+			end
 		case '*'
 			command docker $argv
 	end
