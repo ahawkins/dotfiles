376549eccf448264ae48263433f93bd8f5f2c005
Adam Hawkins <hi@ahawkins.me>
2016-03-25 00:43:09 +0530

    Add direnv integration

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,9 @@ set PATH "$HOME/bin" $PATH
 
 # Put my bin dir on path
 set PATH "$HOME/bindir/bin" $PATH
+
+# Shutup direnv
+set -x DIRENV_LOG_FORMAT
+
+# Run hook for per-directory env variables
+eval (direnv hook fish)
