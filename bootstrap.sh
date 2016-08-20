ef79d7876f5a32232ff67f94f0d432dda39f98b2
Adam Hawkins <hi@ahawkins.me>
2016-08-20 12:12:49 +0530

    Add bootstrap fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,27 +4,25 @@ set -eou pipefail
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Linking configs"
+echo "Linking configs from ${script_dir}"
 mkdir -p ~/.config
-ln -sf "$script_dir/tmux" ~/.tmux
+ln -sf "${script_dir}/tmux" ~/.tmux.conf
 
-ln -sf "$script_dir/gitignore" ~/.gitignore
-ln -sf "$script_dir/gitconfig" ~/.gitconfig
+ln -sf "${script_dir}/gitignore" ~/.gitignore
+ln -sf "${script_dir}/gitconfig" ~/.gitconfig
 
 rm -rf ~/.config/fish
-ln -sf "$script_dir/fish/" ~/.config/fish
+ln -sf "${script_dir}/fish/" ~/.config/fish
 
 rm -rf ~/.vimrc
-rm -rf ~/.vim
-mkdir -p ~/.vim
 
-ln -sf "$script_dir/vim/functions" ~/.vim/functions
-ln -sf "$script_dir/vim/ftplugin" ~/.vim/ftplugin
-ln -sf "$script_dir/vim/plugin" ~/.vim/plugin
-ln -sf "$script_dir/vim/rc" ~/.vim/rc
-ln -sf "$script_dir/vim/vimrc" ~/.vimrc
+ln -sf "${script_dir}/vim/functions" ~/.vim/functions
+ln -sf "${script_dir}/vim/ftplugin" ~/.vim/ftplugin
+ln -sf "${script_dir}/vim/plugin" ~/.vim/plugin
+ln -sf "${script_dir}/vim/rc" ~/.vim/rc
+ln -sf "${script_dir}/vim/vimrc" ~/.vimrc
 
-if [ "$SHELL" !=  "$(which fish)" ]; then
+if [ "${SHELL}" != "$(which fish)" ]; then
   echo "Time to enter Fish"
   chsh -s $(which fish)
 fi
