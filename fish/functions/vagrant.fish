f62e752296d1dd862f6647739486457b07c601fb
Adam Hawkins <hi@ahawkins.me>
2016-08-20 12:10:23 +0530

    Split long lines with \

--- fish/functions/vagrant.fish
+++ fish/functions/vagrant.fish
@@ -1,7 +1,10 @@
 function vagrant
 	switch $argv[1]
 		case teardown
-			command vagrant global-status | grep running | awk '{print $1}' | xargs -L1 vagrant halt
+			command vagrant global-status \
+				| grep running \
+				| awk '{print $1}' \
+				| xargs -L1 vagrant halt
 		case '*'
 			command vagrant $argv
 	end
