d98f245af380c141b204eddd3bfa12b8bfd94e8e
Adam Hawkins <hi@ahawkins.me>
2016-08-20 12:12:14 +0530

    Update config.fish with misc fixes

--- fish/config.fish
+++ fish/config.fish
@@ -2,20 +2,27 @@
 # if this computer hasn't been prepared before
 set -q fish_setup_done; or fish_setup
 
-# Allow each machine to specify unique configuration
-# information
-if test -f ~/.computer
-  . ~/.computer
+set -x EDITOR vim
+set -x S_SRC_DIR ~/src
+
+# Put my bin dir on path
+if test -d "$S_SRC_DIR/ahawkins/bindir/bin"
+  set PATH "$S_SRC_DIR/ahawkins/bindir/bin" $PATH
 end
 
 # Local Bin
-set PATH "$HOME/bin" $PATH
-
-# Put my bin dir on path
-set PATH "$HOME/bindir/bin" $PATH
+if test -d "$HOME/bin"
+  set PATH "$HOME/bin" $PATH
+end
 
 # Shutup direnv
 set -x DIRENV_LOG_FORMAT ""
 
 # Run hook for per-directory env variables
 eval (direnv hook fish)
+
+# Allow each machine to specify unique configuration
+# information
+if test -f ~/.computer
+  . ~/.computer
+end
