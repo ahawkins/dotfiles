1cb036c4a161dab394339dc9cebc043261a9c049
Adam Hawkins <hi@ahawkins.me>
2016-11-09 19:57:44 +0530

    Add vagrant nuke
    This command destroys all known vagrants

--- fish/functions/vagrant.fish
+++ fish/functions/vagrant.fish
@@ -5,6 +5,10 @@ function vagrant
 				| grep running \
 				| awk '{print $1}' \
 				| xargs -L1 vagrant halt
+		case nuke
+			command vagrant global-status \
+				| awk '{print $1}' \
+				| xargs -L1 vagrant destroy -f
 		case '*'
 			command vagrant $argv
 	end
