bfdf8cd430c2dd3564eebf5529cf6ba1afe5c965
Adam Hawkins <hi@ahawkins.me>
2016-11-11 19:15:46 +0530

    Add agignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ echo "Linking configs from ${script_dir}"
 mkdir -p ~/.config
 ln -sf "${script_dir}/tmux" ~/.tmux.conf
 
+ln -sf "${script_dir}/agignore" ~/.agignore
+
 ln -sf "${script_dir}/git/gitignore" ~/.gitignore
 ln -sf "${script_dir}/git/gitconfig" ~/.gitconfig
 
@@ -21,6 +23,7 @@ ln -sfh "${script_dir}/vim/ftplugin" ~/.vim/ftplugin
 ln -sfh "${script_dir}/vim/plugin" ~/.vim/plugin
 ln -sfh "${script_dir}/vim/rc" ~/.vim/rc
 ln -sf "${script_dir}/vim/vimrc" ~/.vimrc
+
 
 if [ "${SHELL}" != "$(which fish)" ]; then
   echo "Time to enter Fish"
