347177d69eddba8dede6290ad42954b37460dfa3
Adam Hawkins <hi@ahawkins.me>
2016-11-12 20:00:57 +0530

    Rework to support comp generated config

--- fish/config.fish
+++ fish/config.fish
@@ -2,27 +2,15 @@
 # if this computer hasn't been prepared before
 set -q fish_setup_done; or fish_setup
 
+# Source host specific configuration
+if test -f ~/.config/comp/env.fish
+  . ~/.config/comp/env.fish
+end
+
 set -x EDITOR vim
-set -x S_SRC_DIR ~/src
-
-# Put my bin dir on path
-if test -d "$S_SRC_DIR/ahawkins/bindir/bin"
-  set PATH "$S_SRC_DIR/ahawkins/bindir/bin" $PATH
-end
-
-# Local Bin
-if test -d "$HOME/bin"
-  set PATH "$HOME/bin" $PATH
-end
 
 # Shutup direnv
 set -x DIRENV_LOG_FORMAT ""
 
 # Run hook for per-directory env variables
 eval (direnv hook fish)
-
-# Allow each machine to specify unique configuration
-# information
-if test -f ~/.computer
-  . ~/.computer
-end
