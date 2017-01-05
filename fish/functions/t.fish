64e4846bc0c6ebd40daad8d398c2f70d7a906e05
Adam Hawkins <hi@ahawkins.me>
2017-01-05 11:48:10 +0530

    Move project specific stuff to ahawkins/comp

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,12 +1,3 @@
 function t
-	switch "$argv[1]"
-		case ss # saltside
-			set -x TODO_FILE $TODO_DIR/saltside.txt
-			command todo.sh $argv[2..-1]
-		case sd # slashdeploy
-			set -x TODO_FILE $TODO_DIR/slashdeploy.txt
-			command todo.sh $argv[2..-1]
-		case '*'
-			command todo.sh $argv
-	end
+	command todo.sh $argv
 end
