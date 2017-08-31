9615e21dc0362c557db8a7dfce963bf8d02e8e1c
Adam Hawkins <hi@ahawkins.me>
2017-08-31 13:20:45 +0530

    fix(fish): group conf.d dependencies

--- fish/.config/fish/conf.d/ev.fish
+++
@@ -1,4 +0,0 @@
-if type -q ev
-	ev gnupg > /dev/null
-	ev comp > /dev/null
-end
--- fish/.config/fish/conf.d/fundle.fish
+++ fish/.config/fish/conf.d/fundle.fish
@@ -1,4 +1,9 @@
 if type -q fundle
-  fundle plugin ahawkins/ev-fish --url 'https://github.com/ahawkins/ev-fish.git#bugfix/replace-ls'
+	fundle plugin ahawkins/ev-fish --url 'https://github.com/ahawkins/ev-fish.git#bugfix/replace-ls'
 	fundle init
+end
+
+if type -q ev
+	ev gnupg > /dev/null
+	ev comp > /dev/null
 end
