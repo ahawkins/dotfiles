6cca5470cfad4c53e6a5d2659b5917ae35d1188d
Adam Hawkins <hi@ahawkins.me>
2018-12-15 20:52:16 +0530

    ref(*): rework 256 color test

--- bin/.local/bin/256colors.py
+++ bin/.local/bin/256colors.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # Ported to Python from http://www.vim.org/scripts/script.php?script_id=1349
 
-print "Color indexes should be drawn in bold text of the same color."
+print("Color indexes should be drawn in bold text of the same color.")
 print
 
 colored = [0] + [0x5f + 40 * n for n in range(0, 5)]
 colored_palette = [
-	"%02x/%02x/%02x" % (r, g, b) 
+	"#%02x%02x%02x" % (r, g, b) 
 	for r in colored
 	for g in colored
 	for b in colored
@@ -14,7 +14,7 @@ colored_palette = [
 
 grayscale = [0x08 + 10 * n for n in range(0, 24)]
 grayscale_palette = [
-	"%02x/%02x/%02x" % (a, a, a)
+	"#%02x%02x%02x" % (a, a, a)
 	for a in grayscale 
 ]
 
@@ -25,5 +25,5 @@ reset = "\033[0m"
 for (i, color) in enumerate(colored_palette + grayscale_palette, 16):
 	index = (bold + "%4s" + reset) % (i, str(i) + ':')
 	hex   = (normal + "%s" + reset) % (i, color)
-	newline = '\n' if i % 6 == 3 else ''
-	print index, hex, newline, 
+	newline = '\n' if i % 6 == 3 else ' '
+	print(index, hex, end=newline)
