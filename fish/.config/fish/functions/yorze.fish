0c0c6a93c12e893528f7f318dabed844f5c375ad
Adam Hawkins <hi@ahawkins.me>
2018-12-15 20:48:06 +0530

    fix(fish): sync yorze path with github org

--- fish/.config/fish/functions/yorze.fish
+++ fish/.config/fish/functions/yorze.fish
@@ -7,17 +7,17 @@ function yorze
 			set start_month $_flag_month
 			set end_month (math "$_flag_month+1")
 
-			pushd $S_SRC_DIR/yorze/mono
+			pushd $S_SRC_DIR/yorzegroup/mono
 			git log \
 				--since "2018-$start_month-01" \
 				--until "2018-$end_month-01" \
-				--pretty format:'%ad - %h - %s' \
+				--pretty=format:'%ad - %h - %s' \
 				--date short
 			popd
 		case dev
 			set --erase argv[1]
 
-			pushd $S_SRC_DIR/yorze/mono
+			pushd $S_SRC_DIR/yorzegroup/mono
 			script/deploy-dev $argv
 			popd
 		case '*'
