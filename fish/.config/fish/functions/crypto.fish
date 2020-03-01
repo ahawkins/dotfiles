3894ddc5ab1fd53255c452f3eb8904d36bcf48d2
Adam Hawkins <adam@hawkins.io>
2020-03-01 12:49:00 -1000

    feat(*): update GPG keys

--- fish/.config/fish/functions/crypto.fish
+++ fish/.config/fish/functions/crypto.fish
@@ -6,9 +6,9 @@ function crypto --description 'gpg/crypto helpers'
 		case ssh
 			ssh-add -L | grep --color=never -Fi 'cardno'
 		case gpg
-			gpg -a --export 'hi@ahawkins.me'
+			gpg -a --export 'adam@hawkins.io'
 		case '*'
-			echo "Unknown command $argv[1]" ^&1
+			echo "Unknown command $argv[1]" 1>&2
 			return 1
 	end
 end
