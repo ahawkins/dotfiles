3e25acf9a874c7c9578e5b450c422fcba4814638
Adam Hawkins <adam@hawkins.io>
2020-03-01 12:51:32 -1000

    feat(*): prefer skillshare-workstation utils

--- fish/.config/fish/functions/sk.fish
+++ fish/.config/fish/functions/sk.fish
@@ -1,3 +1,14 @@
-function sk --wraps skillshare
-	command skillshare $argv
+function sk --wraps skillshare-workstation
+	switch "$argv[1]"
+		case tt
+			set --erase argv[1]
+			set --export TIMETRAP_SHEET skillshare
+
+			timetrap $argv
+		case gh
+			set --erase argv[1]
+			curl -u "ahawkins:$SKILLSHARE_GITHUB_ACCESS_TOKEN" $argv
+		case '*'
+			command skillshare-workstation $argv
+	end
 end
