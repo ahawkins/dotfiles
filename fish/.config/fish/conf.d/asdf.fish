fadd08cf50aebb787cf2a86557e947d6433074d3
Adam Hawkins <adam@hawkins.io>
2021-02-20 12:25:29 -1000

    refactor(fish): group all PATH related things
    This makes it easier to control ordering.

--- fish/.config/fish/conf.d/asdf.fish
+++
@@ -1,5 +0,0 @@
-if test -f ~/.asdf/asdf.fish
-	source ~/.asdf/asdf.fish
-
-	ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
-end
--- fish/.config/fish/conf.d/path.fish
+++ fish/.config/fish/conf.d/path.fish
@@ -2,16 +2,20 @@ if not string match ~/.local/bin $PATH > /dev/null
 	set -x PATH ~/.local/bin $PATH
 end
 
-if not string match /usr/local/bin $PATH > /dev/null
-	set -x PATH $PATH /usr/local/bin
-end
-
-if not string match /usr/local/sbin $PATH > /dev/null
-	set -x PATH $PATH /usr/local/sbin
-end
-
 if test -d ~/.local/comp/bin
 	if not string match ~/.local/comp/bin $PATH > /dev/null
 		set -x PATH ~/.local/comp/bin $PATH
 	end
+end
+
+set -x YARN_PREFIX $HOME/.yarn
+
+if not string match $YARN_PREFIX/bin $PATH > /dev/null
+	set -x PATH $YARN_PREFIX/bin $PATH
+end
+
+if test -f ~/.asdf/asdf.fish
+	source ~/.asdf/asdf.fish
+
+	ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
 end
