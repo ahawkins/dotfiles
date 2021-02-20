52c0cc4ac457b7d9f1ceb3f752519938827df6df
Adam Hawkins <adam@hawkins.io>
2021-02-20 12:10:36 -1000

    feat(fish): remove unneeded hack

--- fish/.config/fish/conf.d/tokend_yubikey_hack.fish
+++
@@ -1,4 +0,0 @@
-if ps aux | grep -qE '^_tokend.+Yubikey'
-	security find-generic-password -s comp.yubikey.sudo_password -w -a $USER \
-		| sudo -S kill (ps aux | grep -E '^_tokend.+Yubikey' | awk '{print $2}') ^ /dev/null
-end
