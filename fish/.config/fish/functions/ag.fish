46ddc56dc30debebf4c9401d73902d63d7d3b85a
Adam Hawkins <adam@hawkins.io>
2021-03-13 20:35:39 -1000

    feat(fish): remove unused custom functions

--- fish/.config/fish/functions/ag.fish
+++
@@ -1,3 +0,0 @@
-function ag -w ag
-	command ag --path-to-ignore ~/.agignore $argv
-end
--- fish/.config/fish/functions/t.fish
+++
@@ -1,7 +0,0 @@
-function t
-	if not set --query argv[1]
-		command todo.sh ls
-	else
-		command todo.sh $argv
-	end
-end
