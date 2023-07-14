575b30cb38fe41735ad7128aee7064ae9225062c
Adam Hawkins <adam@hawkins.io>
2023-07-14 13:46:13 -1000

    feat(fish): add more skillshare shortcuts

--- fish/.config/fish/functions/sk.fish
+++ fish/.config/fish/functions/sk.fish
@@ -9,6 +9,12 @@ function sk --wraps skillshare-workstation
 		case gh
 			set --erase argv[1]
 			curl -u "ahawkins:$SKILLSHARE_GITHUB_ACCESS_TOKEN" $argv
+		case dd
+			set --erase argv[1]
+			curl -H "DD-API-KEY: $DD_API_KEY" -H "DD-APPLICATION-KEY: $DD_APP_KEY" $argv
+		case cf
+			set --erase argv[1]
+			curl -H "Authorization: $CF_API_KEY" $argv
 		case '*'
 			command skillshare-workstation $argv
 	end
