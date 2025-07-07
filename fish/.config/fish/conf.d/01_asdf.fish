6e075227cac166b672a2c4cf813b219720fbd808
Adam Hawkins <adam@hawkins.io>
2025-07-07 08:57:13 -1000

    refactor(fish): prefer builtin

--- fish/.config/fish/conf.d/01_asdf.fish
+++ fish/.config/fish/conf.d/01_asdf.fish
@@ -1,5 +1,6 @@
 # Configure ASDF for use with https://github.com/asdf-community/asdf-direnv
 if test -f ~/.asdf/asdf.fish
-	fish_append_path ~/.asdf/bin
+	# Only add the asdf executable itself, do not source everything
+	fish_add_path ~/.asdf/bin
 	ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
 end
