ac2b8aa7f9d1a21876bfbe3456dc6dcdee371b05
Adam Hawkins <adam@hawkins.io>
2025-07-07 10:29:00 -1000

    feat(fish): support multiple comp files
    Create a "env.d" directory where comp can drop multiple files. This
    allows common files and host specific shell configurations.

--- fish/.config/fish/conf.d/03_comp.fish
+++ fish/.config/fish/conf.d/03_comp.fish
@@ -1,6 +1,11 @@
-# Source host specific configuration
+# see https://github.com/ahawkins/comp
+
 if test -f ~/.config/comp/env.fish
   source ~/.config/comp/env.fish
+end
+
+for file in (path sort ~/.config/comp/env.d/*.fish)
+	source $file
 end
 
 if test -d $EVPATH/comp
