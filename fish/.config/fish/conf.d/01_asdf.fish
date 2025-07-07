fe0ba6f4272d0924a886db2bb56b0be0ac052500
Adam Hawkins <adam@hawkins.io>
2025-07-07 11:35:45 -1000

    fix(fish): prefer --global fish_user_paths
    This matches the behavior of brew's shellenv command. It explicitly uses
    --global option. This will make global to the current shell. This avoids
    potential confusing behavior when using fish_user_paths without the -g
    for triggering default behavior.

--- fish/.config/fish/conf.d/01_asdf.fish
+++ fish/.config/fish/conf.d/01_asdf.fish
@@ -1,6 +1,6 @@
 # Configure ASDF for use with https://github.com/asdf-community/asdf-direnv
 if test -f ~/.asdf/asdf.fish
 	# Only add the asdf executable itself, do not source everything
-	fish_add_path ~/.asdf/bin
+	fish_add_path -g ~/.asdf/bin
 	ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
 end
