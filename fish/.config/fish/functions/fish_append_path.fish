b65a7e531dffa55bbbae728f3eea1cb1806a3c4f
Adam Hawkins <adam@hawkins.io>
2025-07-07 09:02:00 -1000

    chore(fish): rmeove dead functions
    Use fish_add_path instead.

--- fish/.config/fish/functions/fish_append_path.fish
+++
@@ -1,5 +0,0 @@
-function fish_append_path
-	if not string match -q $PATH $argv[1]
-		set -x PATH $PATH $argv[1]
-	end
-end
--- fish/.config/fish/functions/fish_prepend_path.fish
+++
@@ -1,5 +0,0 @@
-function fish_prepend_path
-	if not string match -q $PATH $argv[1]
-		set -x PATH $argv[1] $PATH
-	end
-end
